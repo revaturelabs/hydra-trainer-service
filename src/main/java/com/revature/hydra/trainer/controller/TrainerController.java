0a8c7b97a1bff261c6c3dfd33be5527ce58faa67
Bryton Hall <email@bryton.io>
2018-04-01 23:42:37 -0400

    simplify request mapping annotations

--- src/main/java/com/revature/hydra/trainer/controller/TrainerController.java
+++ src/main/java/com/revature/hydra/trainer/controller/TrainerController.java
@@ -7,23 +7,21 @@ import javax.validation.Valid;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.hydra.trainer.model.SimpleTrainer;
 import com.revature.hydra.trainer.model.Trainer;
 import com.revature.hydra.trainer.model.TrainerRole;
-import com.revature.hydra.trainer.repository.TrainerRepository;
 import com.revature.hydra.trainer.service.TrainerCompositionService;
 
 @RestController
@@ -35,6 +33,9 @@ public class TrainerController {
 	@Autowired
 	private TrainerCompositionService trainerCompositionService;
 
+	@Autowired
+	TrainerController() {}
+
 	/**
 	 * Create trainer
 	 *
@@ -42,7 +43,7 @@ public class TrainerController {
 	 *
 	 * @return the response entity
 	 */
-	@RequestMapping(value = "/vp/trainer/create", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping("/vp/trainer/create")
 	// @PreAuthorize("hasAnyRole('VP')")
 	public ResponseEntity<Trainer> createTrainer(@Valid @RequestBody Trainer trainer) {
 		log.info("Saving trainer: " + trainer);
@@ -57,7 +58,7 @@ public class TrainerController {
 	 *
 	 * @return the response entity
 	 */
-	@RequestMapping(value = "/vp/trainer/update", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@PutMapping("/vp/trainer/update")
 	// @PreAuthorize("hasAnyRole('VP')")
 	public ResponseEntity<Void> updateTrainer(@Valid @RequestBody Trainer trainer) {
 		log.info("Updating trainer: " + trainer);
@@ -72,7 +73,7 @@ public class TrainerController {
 	 * @param email
 	 * @return
 	 */
-	@RequestMapping(value = "/training/trainer/byemail/{email}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping("/training/trainer/byemail/{email}")
 	// @PreAuthorize("permitAll")
 	public ResponseEntity<Trainer> findTrainer(@PathVariable String email) {
 		log.info("Find trainer by email " + email);
@@ -86,7 +87,7 @@ public class TrainerController {
 	 * @param trainer
 	 * @return response entity
 	 */
-	@RequestMapping(value = "/vp/trainer/delete", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@DeleteMapping("/vp/trainer/delete")
 	// @PreAuthorize("hasAnyRole('VP')")
 	public ResponseEntity<Void> makeInactive(@Valid @RequestBody Trainer trainer) {
 		log.info("Updating trainer: " + trainer);
@@ -100,7 +101,7 @@ public class TrainerController {
 	 *
 	 * @return
 	 */
-	@RequestMapping(value = "/vp/trainer/titles", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping("/vp/trainer/titles")
 	// @PreAuthorize("hasAnyRole('VP', 'TRAINER', 'STAGING', 'QC', 'PANEL')")
 	public ResponseEntity<List<String>> getAllTrainersTitles() {
 		log.info("Fetching all trainers titles");
@@ -113,7 +114,7 @@ public class TrainerController {
 	 *
 	 * @return
 	 */
-	@RequestMapping(value = "/all/trainer/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping("/all/trainer/all")
 	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
 	// @PreAuthorize("hasAnyRole('VP', 'TRAINER', 'STAGING', 'QC', 'PANEL')")
 	public ResponseEntity<List<Trainer>> getAllTrainers() {
