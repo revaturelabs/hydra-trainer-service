39004e7df2165159edee5f85f04e446a323fd77d
Robert Hamel <roberthamel@gmail.com>
2018-04-06 21:47:00 -0400

    fix tests

--- src/test/java/com/revature/hydratrainerservice/HydraTrainerServiceApplicationTests.java
+++ src/test/java/com/revature/hydratrainerservice/HydraTrainerServiceApplicationTests.java
@@ -1,18 +1,21 @@
 package com.revature.hydratrainerservice;
 
+import com.revature.hydra.trainer.HydraTrainerServiceApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes = HydraTrainerServiceApplication.class)
 public class HydraTrainerServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
--- src/test/java/com/revature/hydratrainerservice/trainer/TrainerRepositoryServiceApplicationTest.java
+++ src/test/java/com/revature/hydratrainerservice/trainer/TrainerRepositoryServiceApplicationTest.java
@@ -1,18 +1,21 @@
 package com.revature.hydratrainerservice.trainer;
 
+import com.revature.hydra.trainer.HydraTrainerServiceApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
+@ContextConfiguration(classes = HydraTrainerServiceApplication.class)
 public class TrainerRepositoryServiceApplicationTest {
 
-  @Test
-  public void contextLoads() {
+    @Test
+    public void contextLoads() {
 
-  }
+    }
 }
